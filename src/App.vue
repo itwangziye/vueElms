1236ce857eccc6eb06a765a70bc1932b6b28458d
mmm <itwangziye@163.com>
2018-04-11 18:28:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+export default class App extends Vue {
+
+
+
+}
+</script>
+
+<style>
+body {
+  font-size: 16Px !important;
+}
+
+html,body,#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <span class="testClass">wzy哈哈哈</span>
+    <div class="testDiv">
+      div
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.testClass{
+  font-size: 100px;
+}
+.testDiv{
+  width: 100px;
+  height: 100px;
+  background-color: red;
+  margin: 10px auto;
+}
+</style>
